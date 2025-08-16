96630fd1051dbb2dad854ad5c352441b4d23b725
Carl John B. Villavito <carljohnvillavito2023@gmail.com>
2025-08-16 13:56:52 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import { AuthProvider } from './context/AuthContext.jsx';
+import { InstanceProvider } from './context/InstanceContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthProvider>
-      <App />
+      <InstanceProvider>
+        <App />
+      </InstanceProvider>
     </AuthProvider>
   </React.StrictMode>,
 );
