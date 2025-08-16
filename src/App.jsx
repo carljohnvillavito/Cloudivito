ceca51b7a303ba82541e1a0ee9c87669fbd7bfe1
Carl John B. Villavito <carljohnvillavito2023@gmail.com>
2025-08-16 14:21:14 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Register from './pages/Register';
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
 import PrivateRoute from './components/routing/PrivateRoute';
+import Plans from './pages/Plans';
 
 function App() {
   return (
@@ -17,6 +18,9 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/plans" element={<PrivateRoute />}>
+             <Route path="" element={<Plans />} />
+          </Route>
           
           {/* Protected Route */}
           <Route path="/dashboard" element={<PrivateRoute />}>
